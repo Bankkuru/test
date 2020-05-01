aaec24837b3fb985211c9fd1192473c23d5925ca
Bankkuru <wittawatcheawphoo@gmail.com>
2020-05-01 20:30:34 +0700

    login

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,9 +9,9 @@ function NavBar() {
                 <Navbar bg="primary" variant="dark">
                     <Navbar.Brand href="#home">Let's Shopping!!</Navbar.Brand>
                     <Nav className="mr-auto">
-                        < Nav.Link href="https://my-api-shopping.herokuapp.com/api/products">Home</Nav.Link>
+                        < Nav.Link href="">Home</Nav.Link>
                         <Nav.Link href="https://www.facebook.com/wittawat.seiophoo" target='_blank'>Facebook</Nav.Link>
-                        <Nav.Link href="" target='_blank'>Github</Nav.Link>
+                        <Nav.Link href="https://github.com/Bankkuru/test" target='_blank'>Github</Nav.Link>
                     </Nav>
                 </Navbar>
             </header>
